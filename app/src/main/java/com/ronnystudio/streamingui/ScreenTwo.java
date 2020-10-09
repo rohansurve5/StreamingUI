712ae4705da706275690529a25c6215767228a1d
rohansurve <rohan.surve5@gmail.com>
2020-10-09 11:14:59 +0530

    scree 2 done

---
+++ app/src/main/java/com/ronnystudio/streamingui/ScreenTwo.java
@@ -0,0 +1,14 @@
+package com.ronnystudio.streamingui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScreenTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_two);
+    }
+}
